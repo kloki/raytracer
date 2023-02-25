c81ee6b64e7bd0526ed9c592c1c7c046ae98c016
Koen Klinkers <k.klinkers@gmail.com>
2023-02-25 15:57:53 +0100

    print rgb

---
+++ src/color.rs
@@ -0,0 +1,27 @@
+#[derive(Clone, Debug)]
+pub struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+impl Color {
+    pub fn new(red: u8, green: u8, blue: u8) -> Self {
+        Color { red, green, blue }
+    }
+    pub fn default() -> Self {
+        Color {
+            red: 0,
+            green: 0,
+            blue: 0,
+        }
+    }
+    pub fn set_color(&mut self, red: u8, green: u8, blue: u8) {
+        self.red = red;
+        self.green = green;
+        self.blue = blue;
+    }
+    pub fn to_ppm(&self) -> String {
+        format!("{:3} {:3} {:3}", self.red, self.green, self.blue)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use window::Window;
+mod color;
+mod window;
 fn main() {
-    println!("Hello, world!");
+    let mut screen = Window::new(256, 256);
+
+    for y in 0..255 {
+        for x in 0..255 {
+            screen.pixels[y][x].set_color(
+                x.try_into().unwrap(),
+                (255 - y).try_into().unwrap(),
+                100,
+            );
+        }
+    }
+    println!("{}", screen.to_ppm());
 }
