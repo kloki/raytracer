6d0781995a1b8ec2cc531c05cdad0f4941778adf
Koen Klinkers <k.klinkers@gmail.com>
2023-02-26 18:21:27 +0100

    back to colors

--- src/color.rs
+++ src/color.rs
@@ -1,33 +1,28 @@
 #[derive(Clone, Debug)]
 pub struct Color {
-    red: f32,
-    green: f32,
-    blue: f32,
+    red: u8,
+    green: u8,
+    blue: u8,
 }
 
 impl Color {
     #[allow(dead_code)]
-    pub fn new(red: f32, green: f32, blue: f32) -> Self {
+    pub fn new(red: u8, green: u8, blue: u8) -> Self {
         Color { red, green, blue }
     }
     pub fn default() -> Self {
         Color {
-            red: 0.0,
-            green: 0.0,
-            blue: 0.0,
+            red: 0,
+            green: 0,
+            blue: 0,
         }
     }
     pub fn set_color(&mut self, red: u8, green: u8, blue: u8) {
-        self.red = red as f32 / 255.0;
-        self.green = green as f32 / 255.0;
-        self.blue = blue as f32 / 255.0;
+        self.red = red;
+        self.green = green;
+        self.blue = blue;
     }
     pub fn to_ppm(&self) -> String {
-        format!(
-            "{:3} {:3} {:3}",
-            (255.0 * self.red) as u8,
-            (255.0 * self.green) as u8,
-            (255.0 * self.blue) as u8
-        )
+        format!("{:3} {:3} {:3}", self.red, self.green, self.blue)
     }
 }
